8ced9c474342aa4b844e4a4e39e106b932515161
Amit Narkhede <amit@ccs.neu.edu>
2017-12-12 14:06:19 -0500

    Better solution using promises

--- exercise1/exercise1.js
+++ exercise1/exercise1.js
@@ -1,27 +1,38 @@
 function remoteMathService(cb) {
   var one, two;
-  callOneService(function(err, num) {
-    one = num;
-  });
-  callTwoService(function(err, num) {
-    two = num;
-  });
-  ;
-  return setTimeout(() => {
-    cb(undefined, one + two);
-  }, 1500);
+
+  Promise.all([
+    callOneService(),
+    callTwoService(),
+  ]).then(res => cb(undefined, res[0] + res[1]));
+
+  // return setTimeout(() => {
+  //   cb(undefined, one + two);
+  // }, 1500);
 }
 
-function callOneService(cb) {
-  setTimeout(function() {
-    return cb(undefined, 1);
-  }, 1000);
+function callOneService() {
+  // setTimeout(function() {
+  //   return cb(undefined, 1);
+  // }, 1000);
+
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        return resolve(1);
+    }, 1000);
+  });
 }
 
 function callTwoService(cb) {
-  setTimeout(function() {
-    return cb(undefined, 2);
-  }, 1500);
+  // setTimeout(function() {
+  //   return cb(undefined, 2);
+  // }, 1500);
+
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        return resolve(2);
+    }, 1500);
+  });
 }
 remoteMathService(function(err, answer) {
   if (err) console.log("error ", err);
