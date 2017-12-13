38d629b3ce856118533a9d84ae215e32bd603bc5
Amit Narkhede <amit@ccs.neu.edu>
2017-12-13 11:48:03 -0500

    Display basic information for user details page

--- exercise3/src/components/UserDetails.jsx
+++ exercise3/src/components/UserDetails.jsx
@@ -17,10 +17,80 @@ export class UserDetails extends Component {
     if (!user.loaded) {
       return <h1>Loading...</h1>;
     }
-
+    const userData = user.userData;
     return (
       <div>
-        Hello
+        <h2>User Details for ID {userData.id}</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name : </td>
+              <td>{userData.name}</td>
+            </tr>
+            <tr>
+              <td>Username : </td>
+              <td>{userData.username}</td>
+            </tr>
+            <tr>
+              <td>Email : </td>
+              <td>{userData.email}</td>
+            </tr>
+            <tr>
+              <td>Phone# : </td>
+              <td>{userData.phone}</td>
+            </tr>
+            <tr>
+              <td>Website:</td>
+              <td><a href={userData.website} >{userData.website}</a></td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Address</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Street : </td>
+              <td>{userData.address.street}</td>
+            </tr>
+            <tr>
+              <td>Suite : </td>
+              <td>{userData.address.suite}</td>
+            </tr>
+            <tr>
+              <td>City : </td>
+              <td>{userData.address.city}</td>
+            </tr>
+            <tr>
+              <td>ZipCode : </td>
+              <td>{userData.address.zipcode}</td>
+            </tr>
+            <tr>
+              <td>Latitude : </td>
+              <td>{userData.address.geo.lat}</td>
+            </tr>
+            <tr>
+              <td>Longitude : </td>
+              <td>{userData.address.geo.lng}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Company Details</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name : </td>
+              <td>{userData.company.name}</td>
+            </tr>
+            <tr>
+              <td>Catch Phrase : </td>
+              <td>{userData.company.catchPhrase}</td>
+            </tr>
+            <tr>
+              <td>Description : </td>
+              <td>{userData.company.bs}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
